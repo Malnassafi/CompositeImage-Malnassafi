e220a86b870021895d4ccdae2ac8af8ec320d2b3
Malnassafi <malnassafi@mail.csuchico.edu>
2017-10-28 15:57:20 -0700

    kinda lost ;(

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include"bitmap.h"
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void fileNames();
+vector <vector <Pixel> > avergaeMatrix(vector<vector <Pixel> >);
+Bitmap cheakImage(bool);
+Pixel averageColorPixel(Pixel, Pixel, Pixel);
+int main()
+{
+    Bitmap image;
+    vector <vector <Pixel> > bmp;
+    int file;
+    cout<<"How many Bmp files would you like to use? [must be more than 1 and no more than 10P\n";
+    cin>> file;
+
+
+    return 0;
+}
+
+void fileNames()
+{
+    string fileName1, fileName2, fileName3, fileName4, fileName5, fileName6, fileName7, fileName8, fileName9, fileName10;
+    int num = 0,
+    do{
+        if( file == 
+
+
+}
+
+Bitmap cheakImage(bool validImage)
+{
+    if(validImage == true)
+    {
+        
+    }
+    else if(validImage == false)
+    {
+        
+    }
+}
