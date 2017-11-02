d3e90273f411d1a36c05041f023ded399e188913
Malnassafi <malnassafi@mail.csuchico.edu>
2017-11-01 18:59:30 -0700

    figureing it out

--- main.cpp
+++ main.cpp
@@ -5,40 +5,34 @@
 
 using namespace std;
 
-void fileNames();
-vector <vector <Pixel> > avergaeMatrix(vector<vector <Pixel> >);
-Bitmap cheakImage(bool);
-Pixel averageColorPixel(Pixel, Pixel, Pixel);
+
+//vector <vector <Pixel> > avergaeMatrix(vector<vector <Pixel> >);
+//Pixel averageColorPixel(Pixel, Pixel, Pixel);
 int main()
 {
     Bitmap image;
     vector <vector <Pixel> > bmp;
-    int file;
-    cout<<"How many Bmp files would you like to use? [must be more than 1 and no more than 10P\n";
-    cin>> file;
-
+    string fileName[10];
+    int c = 0;
+    char addFile;
+    bool validImage = image.isImage();
+    do{
+            cout<<"What is the file name that you want to add? [it must be at least two and no more than 10]\n";
+            cin>>fileName[c];
+            image.open(fileName[c]);
+            if(validImage == true)
+            {
+                cout<<"Image "<<c+1<<" has been loaded\n";
+                c++;
+            }
+            else
+            {
+                cout<<"The file couldnt be loaded, the file doesn't exist or isn't a valid bitmap or it isn't the same dimensions (width and height) as the first image loaded\n";
+                cout<<"please put another file name in";
+            }
+            cout<<"Do you want to add another file Y/N\n";
+            cin>>addFile;
+      }while(fileName[c].size()<10 && addFile == 'Y' || addFile == 'y');
 
     return 0;
-}
-
-void fileNames()
-{
-    string fileName1, fileName2, fileName3, fileName4, fileName5, fileName6, fileName7, fileName8, fileName9, fileName10;
-    int num = 0,
-    do{
-        if( file == 
-
-
-}
-
-Bitmap cheakImage(bool validImage)
-{
-    if(validImage == true)
-    {
-        
-    }
-    else if(validImage == false)
-    {
-        
-    }
 }
